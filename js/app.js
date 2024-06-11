2f1eef3829d92a2c709994fca925bad7d47f8b1f
Mohammad Samin-Al-Wasee <167951273+wsa-wtag@users.noreply.github.com>
2024-06-11 14:16:27 +0600

    Add function to add and update to do list

--- js/app.js
+++ js/app.js
@@ -4,10 +4,35 @@ const resetToDoList = () => {
     const toDoList = document.getElementById("to-do-list");
 
     if (toDoList.childElementCount == 1) {
-        const paragraph = document.createElement("p");
-        paragraph.textContent = "No tasks to show.";
-        toDoList.appendChild(paragraph);
+        const emptyPlaceholder = document.createElement("p");
+        emptyPlaceholder.textContent = "No tasks to show.";
+        emptyPlaceholder.id = "empty-placeholder";
+        toDoList.appendChild(emptyPlaceholder);
     }
+}
+
+const addToDo = () => {
+    const toDo = document.getElementById("to-do-description");
+    const toDoList = document.getElementById("to-do-list");
+    const emptyPlaceholder = document.getElementById("empty-placeholder");
+    let toDos;
+
+    if (emptyPlaceholder) {
+        toDoList.removeChild(emptyPlaceholder);
+        toDos = document.createElement("ul");
+        toDos.id = "to-dos";
+        toDoList.appendChild(toDos);
+    } else {
+        toDos = document.getElementById("to-dos");
+    }
+
+    const prevToDo = toDos.lastElementChild;
+    const newId = prevToDo ? prevToDo.id + 1 : 1;
+
+    const newToDo = document.createElement("li");
+    newToDo.textContent = toDo.value;
+    newToDo.id = newId;
+    toDos.appendChild(newToDo);
 }
 
 resetToDoList();
