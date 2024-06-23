7eb8d140dbb91ca625c98ccf3dee480c1932a5ad
Mohammad Samin-Al-Wasee <167951273+wsa-wtag@users.noreply.github.com>
2024-06-23 10:58:42 +0600

    Update format according to figma design

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 const resetToDoList = () => {
     const toDoList = document.getElementById("toDoList");
 
-    if (toDoList.childElementCount == 1) {
+    if (toDoList.childElementCount == 0) {
         const emptyPlaceholder = document.createElement("p");
         emptyPlaceholder.textContent = "No tasks to show.";
         emptyPlaceholder.id = "emptyPlaceholder";
@@ -12,7 +12,7 @@ const resetToDoList = () => {
 }
 
 const addToDo = () => {
-    const toDo = document.getElementById("taskDescription");
+    const taskDescription = document.getElementById("taskDescription");
     const toDoList = document.getElementById("toDoList");
     const emptyPlaceholder = document.getElementById("emptyPlaceholder");
     let toDos;
@@ -26,11 +26,9 @@ const addToDo = () => {
         toDos = document.getElementById("toDos");
     }
 
-    const prevToDo = toDos.lastElementChild;
-    const newId = prevToDo ? prevToDo.id + 1 : 1;
-
+    const newId = `task${Date.now()}`;
     const newToDo = document.createElement("li");
-    newToDo.textContent = toDo.value;
+    newToDo.textContent = taskDescription.value;
     newToDo.id = newId;
     toDos.appendChild(newToDo);
 }
