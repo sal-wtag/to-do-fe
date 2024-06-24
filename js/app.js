efbd3be4b5c4ba63d730f31c4289ee7e1059afc2
Mohammad Samin-Al-Wasee <167951273+wsa-wtag@users.noreply.github.com>
2024-06-24 13:54:16 +0600

    Refactor to do add and delete functionality to use models

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const toDoList = new ToDoList();
+
+document.addEventListener("DOMContentLoaded", () => {
+    const toDoAddButton = document.getElementById(TO_DO_ADD_BUTTON_ID);
+
+    toDoAddButton.addEventListener("click", () => {
+        const toDoForm = document.getElementById(TO_DO_FORM_ID);
+        const toDo = new ToDo(toDoForm);
+
+        toDoList.add(toDo);
+        toDoList.render();
+    });
+
+    toDoList.render();
+});
--- js/models.js
+++ js/models.js
@@ -13,8 +13,37 @@ class ToDo {
     this.createdAt = new Date();
   }
 
-  save = (toDoList) => {
-    toDoList.push(this);
+  toElement = (toDoList) => {
+    const li = document.createElement("li");
+
+    for (let key in this) {
+      if (typeof this[key] === "function") {
+        continue;
+      }
+
+      if (key === "id") {
+        li.id = this[key];
+        continue;
+      }
+
+      const p = document.createElement("p");
+
+      p.textContent = `${key}: ${this[key]}`;
+
+      li.appendChild(p);
+    }
+
+    const button = document.createElement("button");
+
+    button.textContent = "Remove";
+    button.addEventListener("click", () => {
+      toDoList.remove(this.id);
+      toDoList.render();
+    });
+
+    li.appendChild(button);
+
+    return li;
   }
 }
 
@@ -29,5 +58,38 @@ class ToDoList {
 
   remove = (id) => {
     this.toDos = this.toDos.filter(toDo => toDo.id != id);
+  }
+
+  render = () => {
+    const viewSection = document.getElementById(VIEW_SECTION_ID);
+
+    while (viewSection.firstChild) {
+      viewSection.removeChild(viewSection.firstChild);
+    }
+
+    if (this.toDos.length) {
+      this.#renderList(viewSection);
+    } else {
+      this.#renderEmpty(viewSection);
+    }
+  }
+
+  #renderEmpty = (viewSection) => {
+    const p = document.createElement("p");
+    p.textContent = "No tasks to display.";
+
+    viewSection.appendChild(p);
+  }
+
+  #renderList = (viewSection) => {
+    const ul = document.createElement("ul");
+
+    for (let toDo of this.toDos) {
+      const li = toDo.toElement(this);
+
+      ul.appendChild(li);
+    }
+
+    viewSection.appendChild(ul);
   }
 }
