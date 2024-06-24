19b625dd15fffb4cf3dce02c41a0be53d0bf5afa
Mohammad Samin-Al-Wasee <167951273+wsa-wtag@users.noreply.github.com>
2024-06-24 11:38:24 +0600

    Add new to do list model

--- js/models.js
+++ js/models.js
@@ -9,9 +9,25 @@ class ToDo {
     for (let field of fields) {
       this[field.name] = field.value;
     }
+
+    this.createdAt = new Date();
   }
 
   save = (toDoList) => {
     toDoList.push(this);
+  }
+}
+
+class ToDoList {
+  constructor() {
+    this.toDos = [];
+  }
+
+  add = (toDo) => {
+    this.toDos.push(toDo);
+  }
+
+  remove = (id) => {
+    this.toDos = this.toDos.filter(toDo => toDo.id != id);
   }
 }
